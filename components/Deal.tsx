59cf78b84ad0d171fe056440dcbe1a2d5352bbb6
HarmanPreet-Singh-XYT <preetsinghharman27@gmail.com>
2024-06-12 16:12:48 +0530

    Fix data read error/ link correction b/w deal & data

--- components/Deal.tsx
+++ components/Deal.tsx
@@ -10,7 +10,7 @@ const Deal = () => {
         <p className='border-b-[1px] leading-[50px] tracking-[1.5px] font-semibold text-[18px]'> Deal of The Day</p>
         <div className='p-[30px] border-[1px] mt-8 rounded-xl overflow-auto snap-x snap-proximity flex gap-20'>
             {deals.map((each,index)=><div key={index} className='flex flex-col rounded-xl min-w-full gap-5 h-auto items-center lg:pl-10 snap-center lg:flex-row'>
-                <a href={each.productLink}><img className='w-[100%] h-[100%] min-w-[200px] rounded-md' src={each.imgLink}/></a>
+                <a href={`/deals/${each.productID}`}><img className='w-[100%] h-[100%] min-w-[200px] rounded-md' src={each.imgLink}/></a>
                 <div className='flex flex-col gap-4'>
                     <div className='flex items-center gap-2'>
                         <Stars stars={each.stars}/>
